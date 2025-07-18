7284e4327c5dae4dfce025d4d268b11b0ddd84ea
kumaramansingh151 <kumaramansingh151@gmail.com>
2025-07-19 01:31:26 +0530

    Repeated functionality using while loop

--- Function/temperatureConverter.c
+++ Function/temperatureConverter.c
@@ -6,46 +6,55 @@ float fahrenheitToCelsius(float fahrenheit);
 
 int main()
 {
-    int choice;
-    float celsius, fahrenheit;
+	int choice = 1;
+	float celsius, fahrenheit;
 
-    printf("Enter 1: Fahrenheit To Celsius\nEnter 2: Celsius To Fahrenheit\n\t\tOption : ");
-    scanf("%d", &choice);
+	while (choice != 0)
+	{
+		clrscr();
+		printf("Enter 1: Fahrenheit To Celsius\nEnter 2: Celsius To Fahrenheit\nenter 0: for exit \n\t\tOption : ");
+		scanf("%d", &choice);
 
-    switch (choice)
-    {
-    case 1:
-        printf("\nEnter temperature in Fahrenheit: ");
-        scanf("%f", &fahrenheit);
-        celsius = fahrenheitToCelsius(fahrenheit);
-        printf("\nTemperature in celsius = %.2f°C\n", celsius);
-        break;
+		switch (choice)
+		{
+		case 0:
+			return 0;
 
-    case 2:
-        printf("\nEnter temperature in Celsius: ");
-        scanf("%f", &celsius);
-        fahrenheit = celsiusToFahrenheit(celsius);
-        printf("\nTemperature in Fahrenheit = %.2f°F\n", fahrenheit);
-        break;
+		case 1:
+			printf("\nEnter temperature in Fahrenheit: ");
+			scanf("%f", &fahrenheit);
+			celsius = fahrenheitToCelsius(fahrenheit);
+			printf("\nTemperature in celsius = %.2fÂ°C\n", celsius);
+			getch();
+			break;
 
-    default:
-        printf("Not a correct choise");
-    }
+		case 2:
+			printf("\nEnter temperature in Celsius: ");
+			scanf("%f", &celsius);
+			fahrenheit = celsiusToFahrenheit(celsius);
+			printf("\nTemperature in Fahrenheit = %.2fÂ°F\n", fahrenheit);
+			getch();
+			break;
 
-    getch();
-    return 0;
+		default:
+			printf("Not a correct choise");
+			getch();
+		}
+	}
+	getch();
+	return 0;
 }
 
 /*Function to convert Celsius to Fahrenheit
-Fahrenheit (F)=(Celsius×9/5)+32*/
+Fahrenheit (F)=(CelsiusÃ—9/5)+32*/
 float celsiusToFahrenheit(float celsius)
 {
-    return (celsius * 9 / 5) + 32;
+	return (celsius * 9 / 5) + 32;
 }
 
 /*Function to convert Fahrenheit to Celsius
-Celsius (°C)=(Fahrenheit (°F)-32)×9/5*/
+Celsius (Â°C)=(Fahrenheit (Â°F)-32)Ã—9/5*/
 float fahrenheitToCelsius(float fahrenheit)
 {
-    return (fahrenheit - 32) * 5 / 9;
+	return (fahrenheit - 32) * 5 / 9;
 }
