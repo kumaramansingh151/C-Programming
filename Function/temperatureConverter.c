51ce58671fd7867a7aa9d0dfb019d1e7e76318a8
kumaramansingh151 <kumaramansingh151@gmail.com>
2025-07-18 16:08:41 +0530

    Temperature converter

---
+++ Function/temperatureConverter.c
@@ -0,0 +1,67 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+float celsiusToFahrenheit(float celsius);
+float fahrenheitToCelsius(float fahrenheit);
+
+int main()
+{
+    int n;
+    float celsius, fahrenheit;
+     
+    printf("Enter 1: Fahrenheit To Celsius\nEnter 2: Celsius To Fahrenheit\n           Option : ");
+    scanf("%d",&n);
+     
+    if(n==1)
+    {
+        switch(n)
+        {
+            case 1:
+                 printf("\nEnter temperature in Celsius: ");
+                 scanf("%f",&celsius);
+                 
+                 fahrenheit = celsiusToFahrenheit(celsius);
+                 
+                 printf("\nTemperature in Fahrenheit = %.2f°F\n", fahrenheit);
+                 
+            break;
+        }
+        
+    }
+    else if (n==2)
+    {
+         switch(n)
+         {
+             case 2:
+                  printf("\nEnter temperature in Fahrenheit: ");
+                  scanf("%f", &fahrenheit);
+                  
+                   celsius =fahrenheitToCelsius(fahrenheit);
+                   
+                   printf("\nTemperature in celsius = %.2f°C\n",celsius);
+             break;
+         }     
+    }
+    else
+    {
+        printf("%dNot correct number",n);
+    }
+    
+    getch();
+    return 0;
+}
+                            /*    Function to convert Celsius to Fahrenheit
+                                  Fahrenheit (F)=(Celsius×9/5)+32       */
+                            
+float celsiusToFahrenheit(float celsius)
+{
+    return (celsius * 9 / 5) + 32;
+}
+
+                            /*    Function to convert Fahrenheit to Celsius
+                                  Celsius (°C)=(Fahrenheit (°F)-32)×9/5       */
+float fahrenheitToCelsius(float fahrenheit)
+{
+    return (fahrenheit - 32) * 5 / 9;
+}
