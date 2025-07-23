88cba7869ef35f149bec92c6b0c2d85f55f2b867
kumaramansingh151 <kumaramansingh151@gmail.com>
2025-07-23 13:16:27 +0530

    Character To Assci

---
+++ Basic_Programs/characterToAssci.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char A;
+    printf("Enter a character variable:");
+    scanf("%c",&A);
+    printf("character to assci value %c is : %d",A,A);
+    
+    getch();
+    return 0;
+}
