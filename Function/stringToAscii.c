ef5e8db702c69c8682fd5b576b6a531d1c07ac7c
kumaramansingh151 <kumaramansingh151@gmail.com>
2025-07-25 00:30:32 +0530

    String to Ascii by  calling function

--- Function/stringToAscii.c
+++ Function/stringToAscii.c
@@ -2,20 +2,26 @@
 #include <conio.h>
 #include <string.h>
 
+void displayAscii(char name[10]);
+
 int main()
 {
-    int i = 0;
     char name[10];
 
     printf("Enter your Name: ");
     scanf("%s", name);
 
+    displayAscii(name);
+
+    getch();
+    return 0;
+}
+void displayAscii(char name[])
+{
+    int i = 0;
     while (name[i] != 0)
     {
         printf("%d ", name[i]);
         i++;
     }
-
-    getch();
-    return 0;
 }
